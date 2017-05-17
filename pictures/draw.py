472232d0dec1c8bfb7d9e0e737fd9a2d0cf79953
ProHiryu <songheqi1996@gmail.com>
2017-05-17 14:40:05 +0800

    draw_all

--- pictures/draw.py
+++ pictures/draw.py
@@ -10,35 +10,37 @@ from pprint import pprint
 style.use('ggplot')
 
 
-def draw_block_size():
+def draw(title):
 
     date = []
-    btc = []
+    y = []
 
-    with open('block-size.json', 'r') as f:
+    filename = title + '.json'
+    with open(filename, 'r') as f:
         data = json.load(f)
     for item in data['values']:
         date.append(item['x'])
-        btc.append(item['y'])
+        y.append(item['y'])
+    ylabel = data['unit']
+    title = data['name']
+    title = title + '\n\n' + data['description']
     fig = plt.figure()
     ax1 = plt.subplot2grid((1, 1), (0, 0))
 
     dateconv = np.vectorize(dt.datetime.fromtimestamp)
     date = dateconv(date)
 
-    ax1.plot_date(date, btc, '-')
+    ax1.plot_date(date, y, '-')
     for label in ax1.xaxis.get_ticklabels():
         label.set_rotation(45)
     ax1.grid(True)  # , color='g', linestyle='-', linewidth=5)
 
     plt.xlabel('Date')
-    plt.ylabel('BTC')
-    plt.ylim(0.5,1.1)
-    plt.title('Average Block Size\nThe average block size in MB')
+    plt.ylabel(ylabel)
+    plt.title(title)
     plt.legend()
-    plt.subplots_adjust(left=0.09, bottom=0.20, right=0.94,
-                        top=0.90, wspace=0.2, hspace=0)
+    # plt.subplots_adjust(left=0.09, bottom=0.20, right=0.94,
+    #                     top=0.90, wspace=0.2, hspace=0)
     plt.show()
 
-
-draw_block_size()
+draw('transactions-per-block')
